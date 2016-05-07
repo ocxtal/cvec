10aef0aa2db7664fa2e4e14b3e3d117904444f7d
Hajime Suzuki <yline5050hs@gmail.com>
2016-05-07 18:42:53 +0900

    fix bench.c

--- bench.c
+++ bench.c
@@ -18,76 +18,49 @@ char *make_random_seq(
 	return(buf);
 }
 
+#define _b(x)	( (x) & 0x1f )
+enum bases {
+	A = 0x01, C = 0x02, G = 0x04, T = 0x08
+};
+char const table[] __attribute__(( aligned(16) )) = {
+	[_b('A')] = A,
+	[_b('C')] = C,
+	[_b('G')] = G,
+	[_b('T')] = T,
+	[_b('U')] = T,
+	[_b('R')] = A | G,
+	[_b('Y')] = C | T,
+	[_b('S')] = G | C,
+	[_b('W')] = A | T,
+	[_b('K')] = G | T,
+	[_b('M')] = A | C,
+	[_b('B')] = C | G | T,
+	[_b('D')] = A | G | T,
+	[_b('H')] = A | C | T,
+	[_b('V')] = A | C | G,
+	[_b('N')] = 0,		/* treat 'N' as a gap */
+	[_b('_')] = 0		/* sentinel */
+};
 
 static inline
 char conv_ascii_to_4bit(
 	char c)
 {
-	/* convert to upper case and subtract offset by 0x40 */
-	#define _b(x)	( (x) & 0x1f )
-
-	/* conversion table */
-	enum bases {
-		A = 0x01, C = 0x02, G = 0x04, T = 0x08
-	};
-	char const table[] = {
-		[_b('A')] = A,
-		[_b('C')] = C,
-		[_b('G')] = G,
-		[_b('T')] = T,
-		[_b('U')] = T,
-		[_b('R')] = A | G,
-		[_b('Y')] = C | T,
-		[_b('S')] = G | C,
-		[_b('W')] = A | T,
-		[_b('K')] = G | T,
-		[_b('M')] = A | C,
-		[_b('B')] = C | G | T,
-		[_b('D')] = A | G | T,
-		[_b('H')] = A | C | T,
-		[_b('V')] = A | C | G,
-		[_b('N')] = 0,		/* treat 'N' as a gap */
-		[_b('_')] = 0		/* sentinel */
-	};
 	return(table[_b(c)]);
-
-	#undef _b
 }
 
 static inline
 cvec_t conv_ascii_to_4bit_vec(
 	cvec_t cv)
 {
-	#define _b(x)	( (x) & 0x1f )
-	enum bases {
-		A = 0x01, C = 0x02, G = 0x04, T = 0x08
-	};
-	char const table[] __attribute__(( aligned(16) )) = {
-		[_b('A')] = A,
-		[_b('C')] = C,
-		[_b('G')] = G,
-		[_b('T')] = T,
-		[_b('U')] = T,
-		[_b('R')] = A | G,
-		[_b('Y')] = C | T,
-		[_b('S')] = G | C,
-		[_b('W')] = A | T,
-		[_b('K')] = G | T,
-		[_b('M')] = A | C,
-		[_b('B')] = C | G | T,
-		[_b('D')] = A | G | T,
-		[_b('H')] = A | C | T,
-		[_b('V')] = A | C | G,
-		[_b('N')] = 0,		/* treat 'N' as a gap */
-		[_b('_')] = 0		/* sentinel */
-	};
-	#undef _b
 	return(_conv_5t_cvec(_conv_a5_cvec(cv), table));
 }
 
+#undef _b
+
 int main(void)
 {
-	int64_t len = 10 * 1024 * 1024;
+	int64_t len = 128 * 1024 * 1024;
 
 	printf("nucleotide sequence conversion benchmark: len(%lld)\n", len);
 
